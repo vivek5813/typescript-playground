44870fbe2a39eb365b09685017252d4785997b74
Pal, Vivek-ND <vivek.pal.-ND@disney.com>
2025-10-27 22:29:42 -0400

    commit

--- Basics/functions.ts
+++ Basics/functions.ts
@@ -1,4 +1,4 @@
-function add(a: number, b: number): number {
+function add2(a: number, b: number): number {
     return a + b;
 }
 
@@ -9,4 +9,25 @@ function log(message: string): void {
 function logAndThrow(errorMessage: string): never { // never -> it will never return anything
     console.log(errorMessage);
     throw new Error(errorMessage);
+}
+
+function performJob(cb: (msg: string) => void) {
+    cb('done');
+}
+
+performJob(log);
+
+type User2 = {
+    name: string;
+    age: number;
+    greet: () => string;
+}
+
+let user4: User2 = {
+    name: 'MAx',
+    age: 10,
+    greet() {
+        console.log('Hello there');
+        return this.name;
+    }
 }
---
+++ Basics/special-types.ts
@@ -0,0 +1,7 @@
+let a: null;
+
+a = null;
+
+//a = 'HI';
+
+let b: undefined;
